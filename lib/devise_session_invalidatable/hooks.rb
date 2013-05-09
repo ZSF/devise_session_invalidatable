a88685bce81a2feae06450d1d4ee2ebaa378c417
Lenny Burdette <lennyburdette@gmail.com>
2013-05-09 10:17:43 -0700

    PathChecker doesn't exist in devise anymore. not sure if it's necessary.

--- lib/devise_session_invalidatable/hooks.rb
+++ lib/devise_session_invalidatable/hooks.rb
@@ -6,8 +6,7 @@ end
 # Check for a valid session when fetching the record from the session
 Warden::Manager.after_set_user :only => :fetch do |record, warden, options|
   scope = options[:scope]
-  path_checker = Devise::PathChecker.new(warden.env, scope)
-  if !path_checker.signing_out? && record && record.session_invalidated?
+  if record && record.session_invalidated?
     warden.logout(scope)
     throw :warden, :scope => scope, :message => "Session no longer valid. Please sign in."
   end
